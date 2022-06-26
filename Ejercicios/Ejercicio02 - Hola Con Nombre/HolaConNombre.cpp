85ca9e62636165cde520e702c34f188092718f17
Sergio Calix <sergiocalix1999@gmail.com>
2022-06-26 00:38:11 -0600

    "Entrar"

---
+++ Ejercicios/Ejercicio02 - Hola Con Nombre/HolaConNombre.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	string nombre;
+	
+	cout << "Ingrese el nombre: ";
+	cin >> nombre;
+	
+	cout << endl;
+	cout << "Su nombre es: " << nombre;
+	cout << endl;
+	
+	return 0;
+}
---
+++ Ejercicios/Ejercicio03 - Variables/Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int numeroEntero;
+	numeroEntero = 15;
+	
+	cout << "Entero: " << numeroEntero << endl;
+	
+	numeroEntero = 18;
+	
+	cout << "Entero modificado: " << numeroEntero;
+	
+	return 0;
+}
