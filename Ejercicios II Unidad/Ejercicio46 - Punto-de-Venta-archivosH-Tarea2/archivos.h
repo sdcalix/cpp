bfecebc1b4435b3e12c20d53ee76aa351fc16fae
Sergio Calix <sergiocalix1999@gmail.com>
2022-07-24 12:46:49 -0600

    Agregando ejercicios

---
+++ Ejercicios II Unidad/Ejercicio46 - Punto-de-Venta-archivosH-Tarea2/archivos.h
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void menu();
+void productos(int opcion);
+void imprimirFactura();
+void agregarProducto(string descripcion, int cantidad, double precio);
---
+++ Ejercicios II Unidad/Ejercicio49 - Funciones-recursivas/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int numeroSecreto = 7;
+
+void adivinarNumeroSecreto(int miNumero)
+{
+    if (miNumero == numeroSecreto)
+    {
+        cout << "Adivinastes!" << endl;
+    }
+    else
+    {
+        cout << "Intento fallido con " << miNumero << endl;
+
+        int otroNumero = 0;
+        cout << "Ingrese otro numero: ";
+        cin >> otroNumero;
+        adivinarNumeroSecreto(otroNumero);
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    adivinarNumeroSecreto(5);
+
+    return 0;
+}
